d3cf81993ddb48d01ccf480c13a8bbda8be2fdb8
relytsatous <tyler.soutas@gmail.com>
2022-07-09 12:24:12 -0600

    Changed the styling for scripture study
    Co-authored-by: David Murdock <davemurdock55@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,47 @@ function popupWindow() {
   popup.classList.toggle("show");
 }
 
-
 function helloThere() {
-    alert("Hello There Person: " + iCount + ".");
+  alert("Hello There Person: " + iCount + ".");
 }
 function wuzzzup() {
-    alert("wuzzzup");
+  alert("wuzzzup");
 }
 
+//Accordion
+var coll = document.getElementsByClassName("nav-btn");
+var i;
 
-// Toggle Overlay Function
-$(".toggle").click(function(event) {
-    event.preventDefault();
-    $("div.overlay").fadeToggle("fast");
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+//Accordion
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+// Collapsible Table //
+$(document).ready(function () {
+  $('[data-toggle="toggle"]').change(function () {
+    $(this).parents().next(".hide").toggle();
+  });
 });
